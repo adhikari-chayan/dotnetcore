b21e17ecd1595b40fe07905d94a2a571e5865751
Chayan Adhikari <adhikari.chayan@gmail.com>
2020-02-20 23:22:57 +0530

    signalr demo

---
+++ signalr/RealTimeCharts_Server/HubConfig/ChartHub.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.SignalR;
+using RealTimeCharts_Server.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RealTimeCharts_Server.HubConfig
+{
+    public class ChartHub : Hub
+    {
+            public async Task BroadcastChartData(List<ChartModel> data) => await Clients.All.SendAsync("broadcastchartdata", data);
+       
+    }
+}
