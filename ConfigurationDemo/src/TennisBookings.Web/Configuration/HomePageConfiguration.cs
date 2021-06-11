5e27448e7abf20163e3b48a48d76542dae65cad0
Chayan Adhikari <chayan.adhikari@ivycomptech.com>
2021-06-11 14:11:40 +0530

    configuration demo and code maze async demo

---
+++ ConfigurationDemo/src/TennisBookings.Web/Configuration/HomePageConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TennisBookings.Web.Configuration
+{
+    public class HomePageConfiguration
+    {
+        public bool EnableGreeting { get; set; }
+        public bool EnableWeatherForecast { get; set; }
+
+        [Required(ErrorMessage = "A title is required for the weather forecast section")]
+        public string ForecastSectionTitle { get; set; }
+    }
+}
---
+++ ConfigurationDemo/src/TennisBookings.Web/External/IProductsApiClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisBookings.Web.External.Models;
+
+namespace TennisBookings.Web.External
+{
+   public interface IProductsApiClient
+    {
+        Task<ProductsApiResult> GetProducts();
+    }
+}
