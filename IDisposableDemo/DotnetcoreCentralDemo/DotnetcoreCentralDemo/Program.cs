fee0425f5cc66461e6ed89a8883dd85436b9f130
Chayan Adhikari <chayan.adhikari@ivycomptech.com>
2021-08-15 02:24:55 +0530

    SOLID and GC related demo apps

---
+++ IDisposableDemo/DotnetcoreCentralDemo/DotnetcoreCentralDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DotnetcoreCentralDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using var serviceProxy = new ServiceProxy(null);
+            serviceProxy.Get();
+
+            serviceProxy.Post("");
+        }
+    }
+}
---
+++ IDisposableDemo/DotnetcoreCentralDemo/DotnetcoreCentralDemo/ServiceProxy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+
+namespace DotnetcoreCentralDemo
+{
+    public class ServiceProxy : IDisposable
+    {
+        private readonly HttpClient httpClient;
+        private bool disposed;
+        public ServiceProxy(IHttpClientFactory httpClientFactory)
+        {
+            httpClient = httpClientFactory.CreateClient();
+        }
+
+        ~ServiceProxy()
+        {
+            //called by GC after GC has disposed all managed objects
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposed)
+            {
+                return; 
+            }
+
+            if (disposing)
+            {
+                //Dispose managed objects
+                httpClient.Dispose();
+            }
+            //Dispose unmanaged objects
+
+            disposed = true;
+             
+        }
+
+        public void Get()   
+        {
+            var response = httpClient.GetAsync("");    
+        }
+
+        public void Post(string request)
+        {
+            var response = httpClient.PostAsync("", new StringContent(request));
+        }
+    }
+}
