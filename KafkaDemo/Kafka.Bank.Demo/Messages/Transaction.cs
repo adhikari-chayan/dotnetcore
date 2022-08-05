b43e24114a7c1bc5d3f4604885b659eb489cf343
Chayan Adhikari <chayan.adhikari@ivycomptech.com>
2022-08-05 23:35:54 +0530

    some re org and adding some PS problems from practice

---
+++ KafkaDemo/Kafka.Bank.Demo/Messages/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Messages
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public decimal Value { get; set; }
+    }
+}
---
+++ KafkaDemo/Kafka.Bank.Demo/Producer.API/Controllers/TransactionController.cs
@@ -0,0 +1,35 @@
+﻿using Messages;
+using Microsoft.AspNetCore.Mvc;
+using Producer.API.Services.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Producer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] DTOs.Transaction transaction, CancellationToken cancellationToken)
+        {
+            var transactionId = Guid.NewGuid();
+            await _transactionService.CreateTransaction(new Transaction()
+            {
+                Id = transactionId,
+                UserId = transaction.UserId,
+                Value = transaction.Value
+            }, cancellationToken);
+
+            return Created($"/transaction/{transactionId}", transactionId);
+        }
+    }
+}
